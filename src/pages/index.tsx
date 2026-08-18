0e6ecec8e465eb0484e166f1ccfdc0f3370215be
Nomekuma <t.gunness35@gmail.com>
2026-08-18 12:30:07 +0400

    chore: update version to 0.1.8 in package.json and index.tsx feat: refactor Arrow components for rotation support in Icon.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -31,7 +31,7 @@ export default function Home() {
                 </Translate>
               </span>
               <Badge className={styles.version} variant="secondary">
-                v0.1.7
+                v0.1.8
               </Badge>
             </div>
             <div className={styles.brand}>
